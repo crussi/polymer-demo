ac4aa402df94fcc38b15b4e0b1cc27e0c1b467db
crussi <chrisrussi@gmail.com>
2015-03-06 19:25:38 -0800

    Just saving state

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,5 +1,5 @@
 Template.home.rendered = function() {
-    document.title = "Wedo | better together";
+    document.title = "Twodo | better together";
     return $("<meta>", {
         name: "description",
         content: "Get things done, together"
